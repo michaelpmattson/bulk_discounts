80338af5e88768da5f62c85417fb05809fae304c
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-19 16:08:53 -0600

    Add total revenue for each top item

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -174,22 +174,11 @@ RSpec.describe 'Merchant Items Index page' do
       end
     end
 
-    xit 'shows total revenue next to each item' do
-
+    it 'shows total revenue next to each item' do
+      within '#top-items' do
+        expectation = @ii_12.unit_price * @ii_12.quantity
+        expect(page).to have_content(expectation)
+      end
     end
   end
 end
-
-
-# Merchant Items Index: 5 most popular items
-#
-# As a merchant
-# When I visit my items index page
-# Then I see the names of the top 5 most popular items ranked by total revenue generated
-# And I see that each item name links to my merchant item show page for that item
-# And I see the total revenue generated next to each item name
-#
-# Notes on Revenue Calculation:
-# - Only invoices with at least one successful transaction should count towards revenue
-# - Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
-# - Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
