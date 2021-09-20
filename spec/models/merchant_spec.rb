8db3fff30eb429de370b2eb1e0d3f5123809ea3a
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-19 22:03:20 -0600

    Add test for best_day instance method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -42,15 +42,14 @@ RSpec.describe Merchant, type: :model do
   describe '#class methods' do
     before(:each) do
       @transaction_s = create(:transaction, result: 'success')
-      @transaction_f = create(:transaction)
-      @merchant = create(:merchant)
-      @disabled_item_1 = create(:item, merchant: @merchant)
-      @disabled_item_2 = create(:item, merchant: @merchant)
-      @enabled_item_1  = create(:item, merchant: @merchant, status: 'Enabled')
-      @enabled_item_2  = create(:item, merchant: @merchant, status: 'Enabled')
-      @invoice_1 = create(:invoice)
+      @merchant_1 = create(:merchant)
+      @enabled_item_1  = create(:item, merchant: @merchant_1, status: 'Enabled')
+      @invoice_1 = create(:invoice, created_at: '2012-03-25 09:54:09 UTC') # Sunday, March 25,2012
       @invoice_1.transactions << @transaction_s
-      @i_i_1 = create(:invoice_item, item: @enabled_item_1, unit_price: 2, quantity: 2, invoice: @invoice_1)
+      @invoice_1_1 = create(:invoice)
+      @transaction_s_1 = create(:transaction, result: 'success')
+      @invoice_1_1.transactions << @transaction_s_1
+      @i_i_1 = create(:invoice_item, item: @enabled_item_1, unit_price: 2, quantity: 2, invoice: @invoice_1_1)
       @i_i_2 = create(:invoice_item, item: @enabled_item_1, unit_price: 2, quantity: 2, invoice: @invoice_1)
       @i_i_3 = create(:invoice_item, item: @enabled_item_1, unit_price: 2, quantity: 2, invoice: @invoice_1)
 
@@ -59,53 +58,75 @@ RSpec.describe Merchant, type: :model do
       @invoice_2 = create(:invoice)
       @transaction_s_2 = create(:transaction, result: 'success')
       @invoice_2.transactions << @transaction_s_2
+      @invoice_3 = create(:invoice, created_at: '2012-03-24 15:54:10 UTC') # Saturday, March 25, 2012
+      @transaction_s_3 = create(:transaction, result: 'success')
+      @invoice_3.transactions << @transaction_s_3
       @i_i_4 = create(:invoice_item, item: @item_2, unit_price: 2, quantity: 1, invoice: @invoice_2)
-      @i_i_5 = create(:invoice_item, item: @item_2, unit_price: 2, quantity: 1, invoice: @invoice_2)
-      @i_i_6 = create(:invoice_item, item: @item_2, unit_price: 2, quantity: 1, invoice: @invoice_2)
+      @i_i_5 = create(:invoice_item, item: @item_2, unit_price: 2, quantity: 1, invoice: @invoice_3)
+      @i_i_6 = create(:invoice_item, item: @item_2, unit_price: 2, quantity: 1, invoice: @invoice_3)
 
       @merchant_3 = create(:merchant, name: "Super Sam", status: 'Enabled')
       @item_3 = create(:item, merchant: @merchant_3)
-      @invoice_3 = create(:invoice)
-      @transaction_s_3 = create(:transaction, result: 'success')
-      @invoice_3.transactions << @transaction_s_3
-      @i_i_7 = create(:invoice_item, item: @item_3, unit_price: 2, quantity: 4, invoice: @invoice_3)
-      @i_i_8 = create(:invoice_item, item: @item_3, unit_price: 2, quantity: 4, invoice: @invoice_3)
-      @i_i_9 = create(:invoice_item, item: @item_3, unit_price: 2, quantity: 4, invoice: @invoice_3)
-
-      @merchant_4 = create(:merchant, name: "Random Rogers", status: 'Enabled')
-      @item_4 = create(:item, merchant: @merchant_4)
       @invoice_4 = create(:invoice)
       @transaction_s_4 = create(:transaction, result: 'success')
       @invoice_4.transactions << @transaction_s_4
-      @i_i_10 = create(:invoice_item, item: @item_4, unit_price: 2, quantity: 3, invoice: @invoice_4)
-      @i_i_11 = create(:invoice_item, item: @item_4, unit_price: 2, quantity: 3, invoice: @invoice_4)
-      @i_i_12 = create(:invoice_item, item: @item_4, unit_price: 2, quantity: 3, invoice: @invoice_4)
-
-      @merchant_5 = create(:merchant, name: "Caras Cupcakes", status: 'Enabled')
-      @item_5 = create(:item, merchant: @merchant_5)
-      @invoice_5 = create(:invoice)
+      @invoice_5 = create(:invoice, created_at: '2012-03-21 13:54:10 UTC') # Wednesday, March 21, 2012
       @transaction_s_5 = create(:transaction, result: 'success')
       @invoice_5.transactions << @transaction_s_5
-      @i_i_13 = create(:invoice_item, item: @item_5, unit_price: 2, quantity: 6, invoice: @invoice_5)
-      @i_i_14 = create(:invoice_item, item: @item_5, unit_price: 2, quantity: 6, invoice: @invoice_5)
-      @i_i_15 = create(:invoice_item, item: @item_5, unit_price: 2, quantity: 6, invoice: @invoice_5)
+      @i_i_7 = create(:invoice_item, item: @item_3, unit_price: 2, quantity: 4, invoice: @invoice_4)
+      @i_i_8 = create(:invoice_item, item: @item_3, unit_price: 2, quantity: 4, invoice: @invoice_5)
+      @i_i_9 = create(:invoice_item, item: @item_3, unit_price: 2, quantity: 4, invoice: @invoice_5)
 
-      @merchant_6 = create(:merchant, name: "Steve Sews", status: 'Enabled')
-      @item_6 = create(:item, merchant: @merchant_6)
+      @merchant_4 = create(:merchant, name: "Random Rogers", status: 'Enabled')
+      @item_4 = create(:item, merchant: @merchant_4)
       @invoice_6 = create(:invoice)
       @transaction_s_6 = create(:transaction, result: 'success')
       @invoice_6.transactions << @transaction_s_6
-      @i_i_16 = create(:invoice_item, item: @item_6, unit_price: 2, quantity: 5, invoice: @invoice_6)
-      @i_i_17 = create(:invoice_item, item: @item_6, unit_price: 2, quantity: 5, invoice: @invoice_6)
-      @i_i_18 = create(:invoice_item, item: @item_6, unit_price: 2, quantity: 5, invoice: @invoice_6)
+      @invoice_7 = create(:invoice, created_at: '2012-03-10 10:54:10 UTC') # Saturday, March 10, 2012
+      @transaction_s_7 = create(:transaction, result: 'success')
+      @invoice_7.transactions << @transaction_s_7
+      @i_i_10 = create(:invoice_item, item: @item_4, unit_price: 2, quantity: 3, invoice: @invoice_6)
+      @i_i_11 = create(:invoice_item, item: @item_4, unit_price: 2, quantity: 3, invoice: @invoice_7)
+      @i_i_12 = create(:invoice_item, item: @item_4, unit_price: 2, quantity: 3, invoice: @invoice_7)
+
+      @merchant_5 = create(:merchant, name: "Caras Cupcakes", status: 'Enabled')
+      @item_5 = create(:item, merchant: @merchant_5)
+      @invoice_8 = create(:invoice)
+      @transaction_s_8 = create(:transaction, result: 'success')
+      @invoice_8.transactions << @transaction_s_8
+      @invoice_9 = create(:invoice, created_at: '2012-03-13 07:54:10 UTC') # Tuesday, March 13, 2012
+      @transaction_s_9 = create(:transaction, result: 'success')
+      @invoice_9.transactions << @transaction_s_9
+      @i_i_13 = create(:invoice_item, item: @item_5, unit_price: 2, quantity: 6, invoice: @invoice_8)
+      @i_i_14 = create(:invoice_item, item: @item_5, unit_price: 2, quantity: 6, invoice: @invoice_9)
+      @i_i_15 = create(:invoice_item, item: @item_5, unit_price: 2, quantity: 6, invoice: @invoice_9)
+
+      @merchant_6 = create(:merchant, name: "Steve Sews", status: 'Enabled')
+      @item_6 = create(:item, merchant: @merchant_6)
+      @invoice_10 = create(:invoice)
+      @transaction_s_10 = create(:transaction, result: 'success')
+      @invoice_10.transactions << @transaction_s_10
+      @invoice_11 = create(:invoice, created_at: '2012-03-22 08:54:10 UTC') # Thurday, March 22, 2012
+      @transaction_s_11 = create(:transaction, result: 'success')
+      @invoice_11.transactions << @transaction_s_11
+      @i_i_16 = create(:invoice_item, item: @item_6, unit_price: 2, quantity: 5, invoice: @invoice_10)
+      @i_i_17 = create(:invoice_item, item: @item_6, unit_price: 2, quantity: 5, invoice: @invoice_11)
+      @i_i_18 = create(:invoice_item, item: @item_6, unit_price: 2, quantity: 5, invoice: @invoice_11)
 
       @merchant_7 = create(:merchant, name: "Vins Vinyl", status: 'Enabled')
       @item_7 = create(:item, merchant: @merchant_7)
-      @invoice_7 = create(:invoice)
-      @invoice_7.transactions << @transaction_f
-      @i_i_19 = create(:invoice_item, item: @item_7, unit_price: 2, quantity: 6, invoice: @invoice_7)
-      @i_i_20 = create(:invoice_item, item: @item_7, unit_price: 2, quantity: 6, invoice: @invoice_7)
-      @i_i_21 = create(:invoice_item, item: @item_7, unit_price: 2, quantity: 6, invoice: @invoice_7)
+      @invoice_12 = create(:invoice, created_at: '2012-03-27 07:54:11 UTC') # Tuesday, March 27, 2012
+      @transaction_f = create(:transaction)
+      @invoice_12.transactions << @transaction_f
+      @invoice_13 = create(:invoice, created_at: '2012-03-22 08:54:10 UTC') # Thurday, March 22, 2012
+      @transaction_s_13 = create(:transaction, result: 'success')
+      @invoice_13.transactions << @transaction_s_13
+      @invoice_14 = create(:invoice, created_at: '2012-03-13 07:54:10 UTC') # Tuesday, March 13, 2012
+      @transaction_s_14 = create(:transaction, result: 'success')
+      @invoice_14.transactions << @transaction_s_14
+      @i_i_19 = create(:invoice_item, item: @item_7, unit_price: 2, quantity: 6, invoice: @invoice_12)
+      @i_i_20 = create(:invoice_item, item: @item_7, unit_price: 2, quantity: 6, invoice: @invoice_13)
+      @i_i_21 = create(:invoice_item, item: @item_7, unit_price: 2, quantity: 6, invoice: @invoice_14)
     end
 
     it '#enabled_merchants' do
@@ -113,11 +134,18 @@ RSpec.describe Merchant, type: :model do
     end
 
     it '#disabled_merchants' do
-      expect(Merchant.disabled_merchants).to eq([@merchant])
+      expect(Merchant.disabled_merchants).to eq([@merchant_1])
     end
 
     it '#top_five' do
-      expect(Merchant.top_five).to eq([@merchant_5, @merchant_6, @merchant_3, @merchant_4, @merchant])
+      expect(Merchant.top_five).to eq([@merchant_5, @merchant_6, @merchant_7, @merchant_3, @merchant_4])
+    end
+
+    describe 'top_five best_day instance method' do
+      it '#best_day' do
+        expect(@merchant_7.best_day).to eq(@invoice_13.created_at)
+        expect(@merchant_1.best_day).to eq(@invoice_1.created_at)
+      end
     end
   end
 
