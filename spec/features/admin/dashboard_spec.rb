e478a5be77e90d4ce535aa3e4985a0422f83000c
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-21 21:46:34 -0600

    Add test for invoice dates and order

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Admin Dashboard page' do
 
     # 6 successful transactions, 1 failed
     @customer_1    = create(:customer)
-    @invoice_1     = create(:invoice, customer_id: @customer_1.id)
+    @invoice_1     = create(:invoice, customer_id: @customer_1.id, created_at: '2012-03-21 13:54:10 UTC') # Wednesday, March 21, 2012
     @invoice_item_1  = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_1.id, status: 1)
     @transaction_1 = create(:transaction, invoice_id: @invoice_1.id, result: 'success')
     @transaction_2 = create(:transaction, invoice_id: @invoice_1.id, result: 'success')
@@ -30,8 +30,8 @@ RSpec.describe 'Admin Dashboard page' do
 
     # 4 successes
     @customer_3    = create(:customer)
-    @invoice_3     = create(:invoice, customer_id: @customer_3.id)
-    @invoice_item_3  = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_3.id, status: 1)
+    @invoice_3     = create(:invoice, customer_id: @customer_3.id, created_at: '2012-03-24 15:54:10 UTC')
+    @invoice_item_3  = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_3.id, status: 1) # Saturday, March 25, 2012
     @transaction_13 = create(:transaction, invoice_id: @invoice_3.id, result: 'success')
     @transaction_14 = create(:transaction, invoice_id: @invoice_3.id, result: 'success')
     @transaction_15 = create(:transaction, invoice_id: @invoice_3.id, result: 'success')
@@ -39,8 +39,8 @@ RSpec.describe 'Admin Dashboard page' do
 
     # 3 successes
     @customer_4    = create(:customer)
-    @invoice_4     = create(:invoice, customer_id: @customer_4.id)
-    @invoice_item_4  = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_4.id)
+    @invoice_4     = create(:invoice, customer_id: @customer_4.id, created_at: '2012-03-25 09:54:09 UTC')
+    @invoice_item_4  = create(:invoice_item, item_id: @item.id, invoice_id: @invoice_4.id) # Sunday, March 25,2012)
     @transaction_17 = create(:transaction, invoice_id: @invoice_4.id, result: 'success')
     @transaction_18 = create(:transaction, invoice_id: @invoice_4.id, result: 'success')
     @transaction_19 = create(:transaction, invoice_id: @invoice_4.id, result: 'success')
@@ -128,16 +128,26 @@ RSpec.describe 'Admin Dashboard page' do
       within('#incomplete-invoices') do
         within("#incomplete-#{@invoice_1.id}") do
           expect(page).to have_link("##{@invoice_1.id}")
+          expect(page).to have_content("Wednesday, March 21, 2012")
         end
         within("#incomplete-#{@invoice_3.id}") do
           expect(page).to have_link("##{@invoice_3.id}")
+          expect(page).to have_content("Saturday, March 24, 2012")
         end
         within("#incomplete-#{@invoice_4.id}") do
           expect(page).to have_link("##{@invoice_4.id}")
+          expect(page).to have_content("Sunday, March 25, 2012")
           click_on "##{@invoice_4.id}"
         end
       end
       expect(current_path).to eq(admin_invoice_path(@invoice_4.id))
+    end
+
+    it 'orders by created at' do
+      within('#incomplete-invoices') do
+        expect("##{@invoice_4.id}").to appear_before("##{@invoice_3.id}")
+        expect("##{@invoice_3.id}").to appear_before("##{@invoice_1.id}")
+      end
     end
   end
 end
