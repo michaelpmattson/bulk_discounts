269ce7731e51a7659a4915255104845d243e3552
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-24 18:27:27 -0600

    Add merchant discount show route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     get  '/items',     to: 'merchant_items#index'
     post '/items',     to: 'merchant_items#create'
 
-    resources :bulk_discounts, only: [:index]
+    resources :bulk_discounts, only: [:index, :show]
     resources :items,     only: [:new, :show, :edit, :update]
   end
 
