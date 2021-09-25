298aeb5f62c4237dd51ecd10c6371e5858ded778
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-24 18:28:10 -0600

    Add discounts_by_merchant_id

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
+
+  describe '.class methods' do
+    describe '.discounts_by_merchant_id(merchant_id)' do
+      it 'finds all discounts by merchant id' do
+        merchant_1       = create(:merchant)
+        bulk_discount_11 = create(:bulk_discount, merchant: merchant_1)
+        bulk_discount_12 = create(:bulk_discount, merchant: merchant_1)
+        discounts = BulkDiscount.discounts_by_merchant_id(merchant_1.id)
+
+        expect(discounts).to eq([bulk_discount_11, bulk_discount_12])
+      end
+    end
+  end
 end
