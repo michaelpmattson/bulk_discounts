82c09fa3746a1463bc56793114584fdbe60fca79
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-26 18:26:47 -0600

    Remove launchy

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -138,11 +138,9 @@ RSpec.describe 'admin invoices show page', type: :feature do
 
     it 'displays total discounted revenue' do
       visit admin_invoice_path(@invoice_1.id)
-      save_and_open_page
       expect(page).to have_content("Total Discounted Revenue: $47.45")
 
       visit admin_invoice_path(@invoice_2.id)
-      save_and_open_page
       expect(page).to have_content("Total Discounted Revenue: $129.25")
     end
   end
