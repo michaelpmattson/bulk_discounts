05910515bab83ae337d6c6dc36b45194650f9f46
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-28 15:17:38 -0600

    Move tests to edit_spec

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -16,16 +16,6 @@ RSpec.describe 'bulk discount show page', type: :feature do
     it 'has a link to edit the discount' do
       click_link('Edit Discount')
       expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant_1, @bulk_discount_11))
-
-      expect(page).to have_field(:bulk_discount_quantity_threshold, with: @bulk_discount_11.quantity_threshold)
-      expect(page).to have_field(:bulk_discount_percentage, with: @bulk_discount_11.percentage)
-
-      fill_in(:bulk_discount_quantity_threshold, with: 35)
-      fill_in(:bulk_discount_percentage,         with: 30)
-      click_on('Update Discount')
-
-      expect(page).to have_content('Quantity Threshold: 35')
-      expect(page).to have_content('Percentage: 30%')
     end
   end
 end
